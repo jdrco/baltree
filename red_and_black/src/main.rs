e2b28bd9f8ad74304a753870c8081653d1bd6412
Antonio Martin-O <antonio2@ualberta.ca>
2024-03-15 12:53:00 -0600

    Saving before going on laptop

--- red_and_black/src/main.rs
+++ red_and_black/src/main.rs
@@ -7,20 +7,23 @@ enum NodeColor {
     Black,
 }
 
-type Tree = Rc<RefCell<TreeNode>>; // Represents a pointer to the root node of a subtree
-type RedBlackTree = Option<Tree>; // Can either be None or a Tree type
+#[derive(Clone, Debug, PartialEq)]
+enum InsertedSide {
+    Right,
+    Left,
+    Root,
+}
 
-/*
-What components do the Red-black tree and AVL tree have in common? Don’t Repeat Yourself! Never, ever repeat yourself – a fundamental idea in programming.
-*/
+type Tree = Rc<RefCell<TreeNode>>;
+type RedBlackTree = Option<Tree>;
 
 #[derive(Clone, Debug, PartialEq)]
 struct TreeNode {
-    pub color: NodeColor, //Red or Black, Red is always assigned to newly added nodes
-    pub key: u32, // Could be a numerical value where right side is >= to root and left is < than root.
-    pub parent: RedBlackTree, // The node directly above the current node in the tree structure
-    left: RedBlackTree, // The left child of the Node
-    right: RedBlackTree, // The right child of the Node
+    pub color: NodeColor,
+    pub key: u32,
+    pub parent: RedBlackTree,
+    left: RedBlackTree,
+    right: RedBlackTree,
 }
 
 impl TreeNode {
@@ -37,76 +40,87 @@ impl TreeNode {
     }
 
     pub fn insert(node: &mut RedBlackTree, data: u32) {
+        let mut inserted_side: InsertedSide = InsertedSide::Root;
         match node {
             Some(node) => {
-                let root_key = (*node).borrow_mut().key;
+                let root_key = (*node).borrow().key;
                 if data >= root_key {
-                    let ref mut right = (*node).borrow_mut().right;
+                    let right: &mut Option<Rc<RefCell<TreeNode>>> = &mut (*node).borrow_mut().right;
                     if right.is_none() {
-                        if let Some(ref mut new_node) = TreeNode::new(data) {
+                        if let Some(ref mut new_node) = &mut TreeNode::new(data) {
                             (*new_node).borrow_mut().parent = Some(Rc::clone(node));
-                            *right = Some(Rc::clone(new_node));
+                            *right = Some(new_node.clone());
+                            inserted_side = InsertedSide::Right;
                         }
-                       // TreeNode::fix()
-                       return;
-                    } 
-                    TreeNode::insert(right, data);
+                    } else {
+                        TreeNode::insert(right, data);
+                    }   
                 } else {
-                    let ref mut left = (*node).borrow_mut().left;
+                    let left: &mut Option<Rc<RefCell<TreeNode>>> = &mut (*node).borrow_mut().left;
                     if left.is_none() {
-                        if let Some(ref mut new_node) = TreeNode::new(data) {
-                            (*new_node).borrow_mut().parent = Some(Rc::clone(node));
-                            *left = Some(Rc::clone(new_node));
+                        if let Some(new_node) = TreeNode::new(data) {
+                            new_node.borrow_mut().parent = Some(Rc::clone(node));
+                            *left = Some(new_node);
+                            inserted_side = InsertedSide::Left;
                         }
-                        // TreeNode::fix()
-                        return;
-                    }
-                    TreeNode::insert(left, data);
+                    } else {
+                        TreeNode::insert(left, data);   
+                    }  
                 }
-            },
+                TreeNode::fix(Rc::clone(node), inserted_side);
+            }
             None => {}
         }
+    }
+
+    pub fn fix(parent: Tree, side: InsertedSide) {
+        let parent = parent.borrow().parent;
+        if parent.is_none() {
+            node.borrow_mut().color = NodeColor::Black;
+        } else {
+            println!("Hello World");
+        }
+  
     }
 
     pub fn disp(node: RedBlackTree) {
         match node {
             Some(node) => {
-                TreeNode::disp((*node).borrow().left.clone());
-                println!("This is node: {}", (*node).borrow().key);
-              
-                if let Some(parent) = (*node).borrow().parent.clone() {
-                    println!("My parent is node: {}", (*parent).borrow().key);
+                TreeNode::disp(node.borrow().left.clone());
+                println!("This is node: {}. The rc count is {}", node.borrow().key, Rc::strong_count(&node));
+
+                if let Some(parent) = node.borrow().parent.clone() {
+                    println!("My parent is node: {}", parent.borrow().key);
                 } else {
-                    println!("My parent is None.")
+                    println!("My parent is None.");
                 }
 
-                if let Some(left) = (*node).borrow().left.clone() {
-                    println!("My left child is node: {}", (*left).borrow().key);
+                if let Some(left) = node.borrow().left.clone() {
+                    println!("My left child is node: {}", left.borrow().key);
                 } else {
-                    println!("My left child is None.")
+                    println!("My left child is None.");
                 }
-                   
-                if let Some(right) = (*node).borrow().right.clone() {
-                    println!("My right child is node: {}", (*right).borrow().key);
+
+                if let Some(right) = node.borrow().right.clone() {
+                    println!("My right child is node: {}", right.borrow().key);
                 } else {
-                    println!("My right child is None.")
+                    println!("My right child is None.");
                 }
-                
-                TreeNode::disp((*node).borrow().right.clone());
-            },
+
+                TreeNode::disp(node.borrow().right.clone());
+            }
             None => {}
         }
     }
 }
 
-
 fn main() {
-    let ref mut node = TreeNode::new(8);
-    TreeNode::insert(node, 4);
-    TreeNode::insert(node, 3);
-    TreeNode::insert(node, 2);
-    TreeNode::insert(node, 7);
-    TreeNode::insert(node, 9);
+    let mut node = TreeNode::new(8);
+    TreeNode::insert(&mut node, 4);
+    TreeNode::insert(&mut node, 3);
+    TreeNode::insert(&mut node, 2);
+    TreeNode::insert(&mut node, 7);
+    TreeNode::insert(&mut node, 9);
 
-    TreeNode::disp((*node).clone());
+    TreeNode::disp(node.clone());
 }
