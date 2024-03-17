9aee4f2076959ba81a98144ba1254c0b7edc9c0f
Jared Drueco <drueco03@gmail.com>
2024-03-16 22:47:50 -0600

    Generalize tree and separate into modules

---
+++ src/red_black_tree.rs
@@ -0,0 +1,9 @@
+use crate::balancing_tree::{BalancingTree, GenericTree, Node, Tree};
+
+pub struct RedBlackTree {
+    pub tree: BalancingTree,
+}
+
+impl RedBlackTree {
+    // RBT-specific methods
+}
