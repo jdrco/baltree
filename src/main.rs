ed4af255892114673e1a9873a446c849e9f94c8e
Jared Drueco <drueco03@gmail.com>
2024-03-20 12:40:51 -0600

    Add CLI input

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,157 @@ mod bs_tree;
 mod rb_tree;
 
 use crate::avl_tree::AVL;
-
 use crate::rb_tree::RedBlack;
+use std::io;
+
+fn avl_interface() {
+    println!("AVL Tree Created!");
+    let mut avl = AVL::new();
+    loop {
+        println!("Enter command:\n1: Add Key to AVL\n2: Delete Key from AVL\n3: Find the number of leaves\n4: Find the height of tree\n5: Print In-Order Tree\n6: Check if Tree is empty\n7: Print Tree Structure\n8: Exit to Main Menu");
+
+        let mut input = String::new();
+        input.clear(); // Clear the input buffer before reading a new value
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim() {
+            "1" => {
+                println!("Enter Key to Insert: ");
+                input.clear(); // Clear the input buffer before reading a new value
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                let key = input.trim().parse::<i32>(); // Attempt to parse the input as an integer
+
+                match key {
+                    Ok(k) => {
+                        if avl.search(k).is_some() {
+                            println!("Key already exists")
+                        } else {
+                            avl.insert(k);
+                            println!("Key {} inserted.", k);
+                        }
+                    },
+                    Err(_) => println!("Please enter a valid integer."),
+                }
+            },
+            "2" => {
+                println!("Enter Key to Delete: ");
+                input.clear(); // Clear the input buffer before reading a new value
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                let key = input.trim().parse::<i32>(); // Attempt to parse the input as an integer
+                
+                match key {
+                    Ok(k) => {
+                        if avl.search(k).is_some() {
+                            avl.delete(k);
+                            println!("Key {} deleted.", k);
+                        } else {
+                            println!("Key does not exist");
+                        }
+                    },
+                    Err(_) => println!("Please enter a valid integer."),
+                }
+            }
+            "3" => {
+                println!("The number of leaves is: {}", avl.tree.count_leaves());
+            },
+            "4" => {
+                println!("The height of the tree is: {}", avl.tree.get_height());
+            },
+            "5" => {
+                println!("The tree when in-order is:\n"); 
+                avl.print_structure();
+            },
+            "6" => {
+                println!("Checking if tree is empty: {}", avl.tree.is_empty());
+            },
+            "7" => {
+                println!("Printing Tree:\n");
+                avl.print_structure()
+            },
+            // Implement other cases similarly...
+            "8" => {
+                println!("Returning to Main Menu.");
+                break;
+            },
+            _ => println!("Invalid input, try again!"),
+        }
+    }
+}
+
+fn rb_interface() {
+    println!("Red Black Tree Created!");
+    let mut rbt = RedBlack::new();
+    loop {
+        println!("Enter command:\n1: Add Key to RBT\n2: Delete Key from RBT\n3: Find the number of leaves\n4: Find the height of tree\n5: Print In-Order Tree\n6: Check if Tree is empty\n7: Print Tree Structure\n8: Exit to Main Menu");
+
+        let mut input = String::new();
+        input.clear(); // Clear the input buffer before reading a new value
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim() {
+            "1" => {
+                println!("Enter Key to Insert: ");
+                input.clear(); // Clear the input buffer before reading a new value
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                let key = input.trim().parse::<i32>(); // Attempt to parse the input as an integer
+
+                match key {
+                    Ok(k) => {
+                        if rbt.search(k).is_some() {
+                            println!("Key already exists")
+                        } else {
+                            rbt.insert(k);
+                            println!("Key {} inserted.", k);
+                        }
+                    },
+                    Err(_) => println!("Please enter a valid integer."),
+                }
+            },
+            "2" => {
+                println!("Enter Key to Delete: ");
+                input.clear(); // Clear the input buffer before reading a new value
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                let key = input.trim().parse::<i32>(); // Attempt to parse the input as an integer
+                
+                match key {
+                    Ok(k) => {
+                        if rbt.search(k).is_some() {
+                            // TODO: handle delete
+                            // rbt.delete(k);
+                            println!("Key {} deleted.", k);
+                        } else {
+                            println!("Key does not exist");
+                        }
+                    },
+                    Err(_) => println!("Please enter a valid integer."),
+                }
+            }
+            "3" => {
+                println!("The number of leaves is: {}", rbt.tree.count_leaves());
+            },
+            "4" => {
+                println!("The height of the tree is: {}", rbt.tree.get_height());
+            },
+            "5" => {
+                println!("The tree when in-order is:\n"); 
+                rbt.print_structure();
+            },
+            "6" => {
+                println!("Checking if tree is empty: {}", rbt.tree.is_empty());
+            },
+            "7" => {
+                println!("Printing Tree:\n");
+                rbt.print_structure()
+            },
+            // Implement other cases similarly...
+            "8" => {
+                println!("Returning to Main Menu.");
+                break;
+            },
+            _ => println!("Invalid input, try again!"),
+        }
+    }
+}
 
 fn main() {
     let mut avl = AVL::new();
@@ -66,4 +215,26 @@ fn main() {
     // println!("Print Tree Structure (After Delete 5):");
     // rb.print_structure();
     println!("--------------------Red Black Tree: End--------------------");
+    println!();
+    println!("--------------------User Input--------------------");
+    loop {
+        println!("Enter command:\n1: Create AVL tree\n2: Create Red Black tree\n3: Quit");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim() {
+            "1" => {
+                avl_interface()
+            }
+            "2" => {
+                rb_interface()
+            }
+            "3" => {
+                println!("Quit");
+                break;
+            }
+            _=> println!("Invalid input, try again!"),
+        }
+    }
 }
